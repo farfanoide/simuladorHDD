37d4b619b6390084be9d3563e5edd91c88c3a88c
Ivan Karl <ivan6258@gmail.com>
2013-06-19 17:58:07 -0300

    adds SCAN

--- algoritmos.py
+++ algoritmos.py
@@ -83,12 +83,12 @@ def SSTF(list_req, init_pos, direction):
     # obtener_proximo -> devuelve indice del proximo
 
 
-def divide_list(list, pos, sort = True):
+def divide_list(list, pos, sort = False):
     """
         divides list into < (pos) >
     """
-    greater = []
     lower = []
+    greater = []
     for req in list:
         if req > pos:
             greater.append(req)
@@ -104,7 +104,7 @@ def SCAN(list, init_pos, direction):
     pf_result = attend_pf(list, init_pos, direction)
     direction = pf_result[2]
     last_pf = pf_result[0][-1]
-    sorted_lists = divide_list(list, last_pf, direction)
+    sorted_lists = divide_list(list, last_pf, True)
     served_list = pf_result[0]
     greater = sorted_lists[0]
     lower = sorted_lists[1]
@@ -128,7 +128,27 @@ def SCAN(list, init_pos, direction):
     movements += momentum(served_list, init_pos)
     return(served_list,movements,not direction)
 
-
+def CSCAN(list, init_pos, direction):
+    pf_result = attend_pf(list, init_pos, direction)
+    last_pf = pf_result[0][-1]
+    sorted_lists = divide_list(list, last_pf, False)
+    greater = sorted_lists[0]
+    lower = sorted_lists[1]
+    served_list = pf_result[0]
+    movements = []
+    if direction:
+        greater.sort()
+        lower.sort()
+        served_list.extend(greater)
+        served_list.extend(lower)
+        movements = momentum(served_list,init_pos)
+    else:
+        greater.sort(reverse = True)
+        lower.sort(reverse = True)
+        served_list.extend(lower)
+        served_list.extend(greater)
+        movements = momentum(served_list,init_pos)
+    return (served_list, movements, direction)
 
 
 
@@ -163,6 +183,6 @@ print "fin"
 lsstf = [-10,20,22,15,-5,-25]
 # print "test divides"
 # print divide_list(lsstf, 150)
-print "test SCAN"
-print SCAN(lsstf,2,False)
+print "test CSCAN"
+print CSCAN(lsstf,2,False)
 print "fin SCAN"
