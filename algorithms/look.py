902507ab7ab32cfd7a213c379ce9b43436d4daf7
Ivan Karl <ivan6258@gmail.com>
2013-06-27 01:27:59 -0300

    fixes errors with page_faults on look and clook

--- algorithms/look.py
+++ algorithms/look.py
@@ -5,7 +5,7 @@ class LOOK(Scheduling):
     def attend_requirements(self, requirements, init_pos, direction):
 
         current_pos    = self.startup(requirements, init_pos)
-        greater, lower = self.divide_list(requirements, current_pos, sort=True)
+        greater, lower = self.divide_list(self.requirements, current_pos, sort=True)
         post_pf_dir    = self.get_end_dir(self.page_faults, init_pos, direction)
         if post_pf_dir:
             self.attended += greater
--- fastest.py
+++ fastest.py
@@ -3,18 +3,49 @@ from simulator import Simulator
 s = Simulator()
 s.init_pos = 0
 s.requirements = [-307, 145, 133, 66, 312, -254, -477, 179, 114]
+print "executeFCFS()", s.executeFCFS(), "end FCFS"
+print "executeSSTF()", s.executeSSTF(), "end SSTF"
+print "executeCLOOK()", s.executeCLOOK(), "end CLOOK"
+print "executeCSCAN()", s.executeCSCAN(), "end CSCAN"
+print "executeLOOK()", s.executeLOOK(), "end LOOK"
+print "executeSCAN()", s.executeSCAN(), "end SCAN"
 
-print s.executeFCFS()
-print s.executeSSTF()
-print s.executeCLOOK()
-print s.executeCSCAN()
-print s.executeLOOK()
-print s.executeSCAN()
+s = Simulator()
+s.init_pos = 0
+s.requirements = [-307, 145, 133, 66, 312, -254, -477, 179, 114]
+print "executeFCFS()", s.executeFCFS(), "end FCFS"
+print "executeSSTF()", s.executeSSTF(), "end SSTF"
+print "executeCLOOK()", s.executeCLOOK(), "end CLOOK"
+print "executeCSCAN()", s.executeCSCAN(), "end CSCAN"
+print "executeLOOK()", s.executeLOOK(), "end LOOK"
+print "executeSCAN()", s.executeSCAN(), "end SCAN"
+
+s = Simulator()
+s.init_pos = 511
 s.requirements = [307, 145, 133, 66, 312, 254, 477, 179, 114]
+print "executeFCFS()", s.executeFCFS(), "end FCFS"
+print "executeSSTF()", s.executeSSTF(), "end SSTF"
+print "executeCLOOK()", s.executeCLOOK(), "end CLOOK"
+print "executeCSCAN()", s.executeCSCAN(), "end CSCAN"
+print "executeLOOK()", s.executeLOOK(), "end LOOK"
+print "executeSCAN()", s.executeSCAN(), "end SCAN"
 
-print s.executeFCFS()
-print s.executeSSTF()
-print s.executeCLOOK()
-print s.executeCSCAN()
-print s.executeLOOK()
-print s.executeSCAN()
+s = Simulator()
+s.requirements = [307, 145, 133, 66, 312, 254, 477, 179, 114]
+s.init_pos = 0
+print "executeFCFS()", s.executeFCFS(), "end FCFS"
+print "executeSSTF()", s.executeSSTF(), "end SSTF"
+print "executeCLOOK()", s.executeCLOOK(), "end CLOOK"
+print "executeCSCAN()", s.executeCSCAN(), "end CSCAN"
+print "executeLOOK()", s.executeLOOK(), "end LOOK"
+print "executeSCAN()", s.executeSCAN(), "end SCAN"
+
+s = Simulator()
+s.requirements = [307, 145, 133, 66, 312, 254, 477, 179, 114]
+s.init_pos = 511
+print "executeFCFS()", s.executeFCFS(), "end FCFS"
+print "executeSSTF()", s.executeSSTF(), "end SSTF"
+print "executeCLOOK()", s.executeCLOOK(), "end CLOOK"
+print "executeCSCAN()", s.executeCSCAN(), "end CSCAN"
+print "executeLOOK()", s.executeLOOK(), "end LOOK"
+print "executeSCAN()", s.executeSCAN(), "end SCAN"
