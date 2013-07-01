4648ad2a166e2c5c22ea0eaa3667b47d3ee40d9c
Ivan Karl <ivan6258@gmail.com>
2013-06-30 22:08:42 -0300

    some blit still missing... help us find itgc 'some blit still missing... help us find it!!! he/'s name is olorin'! he/'s name is olorin

--- classTest.py
+++ classTest.py
@@ -18,17 +18,34 @@ screen.fill(gray)
 lote = Simulator()
 lote.random_list(15)
 lote.add_random_pf(5)
-button = BaseButton(lote, "executeLOOK", (10,10), "gui/img/button_small.png")
-# button = BaseButton(lote, "executeCLOOK", (100,100), "gui/img/button_small.png")
-# screen.blit(button.img, button.rect)
+
+button = BaseButton(lote, (10,100), "executeLOOK", "gui/img/button_small.png")
+button2 = BaseButton(lote, (10,200), "executeCLOOK","gui/img/button_small.png")
+button3 = BaseButton(lote, (10,300), "executeCSCAN","gui/img/button_small.png")
+button4 = BaseButton(lote, (10,400), "executeSCAN","gui/img/button_small.png")
+button5 = BaseButton(lote, (10,500), "executeFCFS","gui/img/button_small.png")
+button6 = BaseButton(lote, (10,600), "executeSSTF","gui/img/button_small.png")
+buttons = []
+buttons.append(button)
+buttons.append(button2)
+buttons.append(button3)
+buttons.append(button4)
+buttons.append(button5)
+buttons.append(button6)
 # screen.blit(button2.img, button2.rect)
 
 #creamos superficie grafico
 g = ScreenAlgorithms((1024,702),black,lote)
 screen.blit(g.graphic_screen,(0,0))
+screen.blit(button.img, button.pos)
+screen.blit(button2.img, button2.pos)
+screen.blit(button3.img, button3.pos)
+screen.blit(button4.img, button4.pos)
+screen.blit(button5.img, button5.pos)
+screen.blit(button6.img, button6.pos)
 # g.print_leyends('SSTF','345', True)
-# g.print_graphic()
 # g.print_reqs_attended() 
+cursor = BaseButton(lote,(10,10))
 
 clock = pygame.time.Clock()
 pygame.display.flip()
@@ -38,26 +55,36 @@ while run:
     for event in pygame.event.get():
         # print event
         if event.type == pygame.MOUSEBUTTONUP:
-          pos = pygame.mouse.get_pos()
+          x,y = pygame.mouse.get_pos()
+          cursor.rect.x = x
+          cursor.rect.y = y
+          for button in buttons:
+              if button.clicked(cursor):
+                print button.img.get_clip()
+                results = button.executeAction()
+                print results
+                reqs = results[0][0]
+                movs = str(results[0][1])
+                dire = results[0][2]
+                g.print_graphic(reqs)
+                screen.blit(g.graphic_screen,(0,0))
+                print button.action
+          # for button in 
+
           # for button in menu.buttons:
           #   if button.img.get_rect().collidepoint(pos):
                   # print button.executeAction()
           # if button.img.get_rect().collidepoint(pos):
             # n = Button()
             # screen.blit(n.img, (200,200))
-        results = button.executeAction()
-        print results
-        reqs = results[0][0]
-        movs = str(results[0][1])
-        dire = results[0][2]
+        # results = button.executeAction()
 
-        g.print_graphic(reqs)
-        g.print_leyends(results[1], movs, dire)
+        # g.print_graphic(reqs)
+        # g.print_leyends(results[1], movs, dire)
             # print button2.executeAction()
 
             
-        screen.blit(g.graphic_screen,(0,0))
-        pygame.display.flip()
+        # pygame.display.flip()
         if event.type == pygame.QUIT:
             run = False
         #faster dubugging
--- gui/buttons.py
+++ gui/buttons.py
@@ -1,15 +1,27 @@
 import pygame
+
 from pygame.locals import *
 
-class BaseButton():
+class BaseButton(pygame.sprite.Sprite):
 
 
-    def __init__(self, obj_class, action, pos, img='img/button_small.png'):
+    def __init__(self, obj_class, pos, action="", img='gui/img/button_small.png'):
+        pygame.sprite.Sprite.__init__(self)
+        self.pos = pos
+        print self.pos
         self.img = pygame.image.load(img)
-        # self.img_active = img_act
-        self.rect = (pos[0], pos[1], self.img.get_width(), self.img.get_height())
+        # self.rect = (pos[0], pos[1], self.img.get_width(), self.img.get_height())
+        self.rect = self.img.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
         self.obj_class = obj_class
         self.action = action
+
+    def clicked(self, rect):
+        if pygame.sprite.collide_rect(self, rect):
+            return True
+        else:
+            return False
 
     def executeAction(self):
         action = getattr(self.obj_class, self.action)
