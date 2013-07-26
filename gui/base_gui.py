854686dfc427c5445a5cb81295b31f34a32c9af8
Ivan Karl <ivan6258@gmail.com>
2013-07-26 18:15:55 -0300

    improves update_sfc to account for child elements

--- gui/base_gui.py
+++ gui/base_gui.py
@@ -3,21 +3,23 @@ import sys
 import pygame
 from pygame.locals import *
 
-class BaseGui():
+class BaseGui(object):
 
 
     _bkg_colour = (0,0,0)
 
     def __init__(self, base_sfc, rect, padding=(0,0,0,0), color=_bkg_colour):
-
+        print rect
         self.start_x  = rect[0]
         self.start_y  = rect[1]
         self.width    = rect[2]
         self.height   = rect[3]
+        self.rect     = rect
         self.base_sfc = base_sfc
         self.padding  = padding
+        self.elements = []
         self.sfc = pygame.Surface((self.width, self.height))
-        self.sfc.fill(color)
+        # self.sfc.fill(color)
         self.base_sfc.blit(self.sfc, rect)   
         pygame.display.update(self.sfc.get_rect())
 
@@ -25,6 +27,9 @@ class BaseGui():
         pass
 
     def update_sfc(self):
+        if (self.elements):
+            for e in self.elements:
+                e.update_sfc()
         self.base_sfc.blit(self.sfc, (self.start_x, self.start_y))
         pygame.display.update(self.base_sfc.get_rect())
 
@@ -38,13 +43,15 @@ class Button(BaseGui):
         self.action = action
         self.img    = pygame.image.load(img)
         rect        = self.img.get_rect()
-        BaseGui.__init__(self, base_sfc, rect)
+        # BaseGui.__init__(self, base_sfc, rect)
+        super(Button, self).__init__(base_sfc, rect)
 
     def clicked(self, pos):
-        if self.size.collidepoint(pos):
+        if self.rect.collidepoint(pos):
             return True
         else:
             return False
+
     def update_sfc(self):
         self.base_sfc.blit(self.img, (self.start_x, self.start_y))
         pygame.display.update(self.base_sfc.get_rect())
@@ -53,17 +60,17 @@ class Menu(BaseGui):
 
 
     def __init__(self, base_sfc, rect, color, buttons, axis):
-        BaseGui.__init__(self, base_sfc, rect, color)
+        super(Menu, self).__init__(base_sfc, rect, color)
         self.initiate_elements(buttons)
         self.populate_sfc(axis)
+        self.update_sfc()
 
 
     # def instantiate_buttons(self, buttons):
     def initiate_elements(self, buttons):
-        self.buttons = []
         for button in buttons:
             b = Button(self.sfc, button['action'], button['img'])
-            self.buttons.append(b)
+            self.elements.append(b)
 
     def populate_sfc(self, axis=True, step=20):
         # self.instantiate_buttons(buttons)
@@ -71,33 +78,25 @@ class Menu(BaseGui):
         # axis = False -> y
         padding = step
         if axis:
-            for button in self.buttons:
+            for button in self.elements:
                 button.start_x = padding
                 button.start_y = 0
-                button.update_sfc()
+                # button.update_sfc()
                 padding += button.img.get_width()+step
         else:
-            for button in self.buttons:
+            for button in self.elements:
                 button.start_x = 0
                 button.start_y = padding
-                button.update_sfc()
+                # button.update_sfc()
                 padding += button.img.get_height()+step
-        self.update_sfc()
+        
 
-# class Main(object):
-
-#     def __init__(self, size):
-#         self.screen = pygame.display.set_mode(screen_size)
-#         self.screen.fill(black)
-#         pygame.display.flip()
+class Screen(BaseGui):
+    """docstring for Screen"""
 
 
-#     def update_elements(self, sfc):
-#         pygame.display.update(sfc)
-
-#     def initiate_elements(self):
-#         menusize = (0, 0, self.screen.get_width(), self.screen.get_height()/8)
-#         self.menu = Menu(self.screen, menusize, white)    
-        # self.update_elements(self.menu)
-
+    def __init__(self, base_sfc, rect, color, elements):
+        super(Screen, self).__init__(base_sfc, rect, color)
+        self.elements = elements
+        self.selected = True
 
--- testppal.py
+++ testppal.py
@@ -3,8 +3,7 @@ from pygame.locals import *
 from gui.guiclasses import *
 from simulator import Simulator
 from gui.screen_algorithms import *
-# pygame.Surface
-# var
+from gui.base_gui import *
 screen_size = (1000, 640)
 red   = (255,0,0)
 green = (0,255,0)
@@ -12,45 +11,30 @@ blue  = (0,0,255)
 white = (255,255,255)
 black = (0,0,0)
 clock = pygame.time.Clock()
-butt_list=[{'action':'sometext', 'img':"img/button.png"},{'action':'sometext', 'img':"img/button.png"},{'action':'sometext', 'img':"img/button.png"},{'action':'sometext', 'img':"img/button.png"}]
+butt_list=[{'action':'esta', 'img':"gui/img/button.png"},{'action':'otra', 'img':"gui/img/button.png"},{'action':'miraqloco', 'img':"gui/img/button.png"},{'action':'otrama', 'img':"gui/img/button.png"}]
 
 pygame.init()
 s = pygame.display.set_mode(screen_size)
 mrect = (0, 0, s.get_width()/3, s.get_height())
 m = Menu(s, mrect, white, butt_list, False)
-# m = Main(screen_size)
-# m.initiate_elements()
-# m.menu.populate_sfc(butt_list)
 pygame.display.flip()
-for button in m.buttons:
-    print button.start_y
-    print button.start_x
-# print screen.get_width()
-
-
 
 
 run = True
 while run:
     clock.tick(30)
     for event in pygame.event.get():
-        # print event
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
-            for button in m.menu.buttons:
+            for button in m.elements:
                 if button.clicked(pos):
-                    print button.img.get_clip()
-            #         print button.img.get_abs_parent().get_rect()
-                    # print super(Button, button).get_rect()
-            
-            # screen.blit(n.img, (200,200))
-            # pygame.display.flip()
+                    print button.action
         if event.type == pygame.QUIT:
             run = False
         #faster dubugging
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             run = False
-# pygame.quit()
-# sys.exit(), 
+pygame.quit()
+sys.exit(), 
 
 
