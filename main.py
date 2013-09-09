db82bc0d8c3a8c51f2f9b674012178705c91e160
Ivan Karl <ivan6258@gmail.com>
2013-09-09 20:30:21 -0300

    settings panel madafaka!

--- main.py
+++ main.py
@@ -14,35 +14,14 @@ from kivy.config import Config, ConfigParser
 # simulator = Simulator()
 # simulator.requirements = [-5, 15, 40, 65, 20, -35, -400, 90, 100]
 
-class SettingsScreen(Settings):
-	
-	def __init__(self, **kargs):
-		super(SettingsScreen,self).__init__(**kargs)
-		self.config = ConfigParser()
-		self.config.read('pymulator.ini')
-		# config.set('graphics','resizable','0')
-		self.settings = Settings()
-		self.settings.add_json_panel('My custom panel', self.config, 'settings.json')
-
-
-# class GraphicsScreen(Screen):
-#     pass
-
-# class GraphicWidget(Widget):
-#     pass
-
-# class MainScreen(Screen):
-#     pass
-
 class PymulatorApp(App):
-	Config.set('graphics','resizable','0')
-	pass
-    # def build(self):
-        # self.root = ScreenManager()
-        # self.root.add_widget(GraphicsScreen(name='graphics'))
-        # self.root.add_widget(MainScreen(name='main'))
+    use_kivy_settings = False
 
+    def build_config(self, config):
+        config.read('pymulator.ini')
 
+    def build_settings(self, settings):
+        settings.add_json_panel('Pymulator', self.config, 'settings.json')
 
 
 if __name__ == '__main__':
