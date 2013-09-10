c8aa7b4a736ef7ba1b642190c345f9b241cb03c4
Ivan Karl <ivan6258@gmail.com>
2013-09-10 02:56:16 -0300

    playing around with the grid

--- main.py
+++ main.py
@@ -9,12 +9,31 @@ from kivy.properties import NumericProperty, ReferenceListProperty,\
 from kivy.clock import Clock
 from simulator import Simulator
 from kivy.config import Config, ConfigParser
+from kivy.graphics import Line, Rectangle
+from kivy.uix.relativelayout import RelativeLayout
 
 
 # simulator = Simulator()
 # simulator.requirements = [-5, 15, 40, 65, 20, -35, -400, 90, 100]
 
+class Graphic(RelativeLayout):
+
+    """Graphic class to print requirements to canvas"""
+
+    def __init__(self, **kwargs):
+        super(Graphic, self).__init__(**kwargs)
+        self.draw_grid()
+
+    def draw_grid(self):
+
+        for x in xrange(100, 550, 50):
+            with self.canvas:
+                Line(points=[x, 0, x, 400], width=1)
+            self.add_widget(Label(text=str(x), pos=(x - 350, 200)))
+
+
 class PymulatorApp(App):
+
     use_kivy_settings = False
 
     def build_config(self, config):
