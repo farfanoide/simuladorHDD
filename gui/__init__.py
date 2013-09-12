040a5aff53f827a3c0450d95c198f362ae179bc9
Ivan Karl <ivan6258@gmail.com>
2013-09-12 20:08:57 -0300

    removes gui folder (img\'s and whatnot)

--- gui/__init__.py
+++
@@ -1,7 +0,0 @@
-from gui.base import BaseGui
-from gui.button import Button
-from gui.inputbox import InputBox
-from gui.menu import Menu
-from gui.graphic import Graphic
-from gui.screen import Screen
-from gui.message import Message
